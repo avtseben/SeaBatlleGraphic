c18bb141e8417ce5b6a19637251b43f5fa2f28d3
avtseben <d00020@mail.ru>
2016-02-12 17:31:37 +0600

    Сделал проверку на выйгрыш. Комьпютер меня сразу же выйграл. Где то есть ошибка приводящая к безконечному цыклу. Во время одной из игр программа зависла, хотя ходы еще были и у меня и у компа

--- core/src/com/mygdx/game/GameManager.java
+++ core/src/com/mygdx/game/GameManager.java
@@ -1,33 +1,49 @@
 package com.mygdx.game;
 
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
-import com.sun.deploy.perf.PerfLabel;
 
 public class GameManager {
 
     private SeaField player1;
     private aiPlayerField player2;//TODO not flexible
     private boolean player1Turn;
+    private boolean gameContinue;
+    private BitmapFont fnt;
+    private String winnerMessage;
 
     public GameManager (SeaField player1, SeaField player2) {
         this.player1 = player1;
         this.player2 = (aiPlayerField)player2;
         player1Turn = true;
+        gameContinue = true;
+        fnt = new BitmapFont(Gdx.files.internal("fnt2.fnt"), Gdx.files.internal("fnt2.png"), false);
     }
     public void render(SpriteBatch batch) {
         update();
+        if (!gameContinue) fnt.draw(batch,winnerMessage,400,500);
     }
     public  void update() {
-        if(player1Turn) {
-            if (player2.clickForStrike())
-                player1Turn = false;
+        if(gameContinue) {
+            if (player1Turn) {
+                if (player2.clickForStrike())
+                    player1Turn = false;
+                if (!player2.haveAliveShips()) {
+                    gameContinue = false;
+                    winnerMessage = "Player 1 is Win!";
+                }
+            } else {
+                int strikeCoordinate[] = player2.doStrike();
+                String strikeEcho = player1.gotStrike(strikeCoordinate[0], strikeCoordinate[1]);
+                player2.hearEcho(strikeCoordinate, strikeEcho);
+                if (!player1.haveAliveShips()) {
+                    gameContinue = false;
+                    winnerMessage = "Player 2 is Win!";
+                }
+                player1Turn = true;
+            }
         }
-        else {
-            int strikeCoordinate[] = player2.doStrike();
-            String strikeEcho = player1.gotStrike(strikeCoordinate[0],strikeCoordinate[1]);
-            player2.hearEcho(strikeCoordinate,strikeEcho);
-
-            player1Turn = true;
-        }
+        else System.out.print("Game Over");
     }
 }
--- core/src/com/mygdx/game/SeaField.java
+++ core/src/com/mygdx/game/SeaField.java
@@ -2,44 +2,45 @@ package com.mygdx.game;
 
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.Texture;
-import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 public abstract class SeaField {
 
-	final static int FIELD_SIZE = 10;
-	final static int CELL_SIZE = 30;
+    final static int FIELD_SIZE = 10;
+    final static int CELL_SIZE = 30;
     int FIELD_SIZE_PIXELS = FIELD_SIZE * CELL_SIZE;
     protected BitmapFont fnt;
     protected String[][] fieldSet;
     protected int x;
     protected int y;
     protected int selCellX;
-	protected int selCellY;
-	protected static Texture cellsTexture;
+    protected int selCellY;
+    protected static Texture cellsTexture;
     protected static Texture seaTexture;
     protected static Texture splashTexture;
     protected static Texture aimTexture;
     protected String playerType;
-    protected int shipSet[] = {4,3,3,2,2,2,1,1,1,1}; //4-Однопалубных 3-двухпалубных 2-трех палубных и 1 -четырехпалубный
+    protected int shipSet[] = {4, 3, 3, 2, 2, 2, 1, 1, 1, 1}; //4-Однопалубных 3-двухпалубных 2-трех палубных и 1 -четырехпалубный
     protected boolean isMine = false;
 
-	public SeaField(int x, int y) {
-		this.x = x;
+    public SeaField(int x, int y) {
+        this.x = x;
         this.y = y;
         fieldSet = new String[FIELD_SIZE][FIELD_SIZE];
         for (int i = 0; i < FIELD_SIZE; i++)
-            for (int j = 0; j < FIELD_SIZE; j++){
-            fieldSet[i][j] = "water";
-        }
+            for (int j = 0; j < FIELD_SIZE; j++) {
+                fieldSet[i][j] = "water";
+            }
         fnt = new BitmapFont(Gdx.files.internal("fnt2.fnt"), Gdx.files.internal("fnt2.png"), false);
         aimTexture = new Texture("pointer.tga");
         cellsTexture = new Texture("Cells.png");
         splashTexture = new Texture("splashCell.png");
         seaTexture = new Texture("FullWater.png");
     }
+
     public void render(SpriteBatch batch) {
         update();
-        batch.draw(seaTexture,x-112,y-44);
+        batch.draw(seaTexture, x - 112, y - 44);
         for (int i = 0; i < FIELD_SIZE; i++) {
             for (int j = 0; j < FIELD_SIZE; j++) {
                 if (fieldSet[i][j] == "ship" && fieldIsMy())
@@ -50,24 +51,25 @@ public abstract class SeaField {
                     batch.draw(splashTexture, x + j * CELL_SIZE, y + i * CELL_SIZE);
             }
         }
-        if(selCellX > -1 && selCellY > -1)
+        if (selCellX > -1 && selCellY > -1)
             batch.draw(aimTexture, x + selCellX * CELL_SIZE, y + selCellY * CELL_SIZE);
     }
+
     public void update() {
         selCellX = (InputHandler.getMouseX() - x) / CELL_SIZE;
         selCellY = (InputHandler.getMouseY() - y) / CELL_SIZE;
-        if(InputHandler.getMouseX() < x || InputHandler.getMouseY() < y || InputHandler.getMouseX() >= x + FIELD_SIZE_PIXELS || InputHandler.getMouseY() >= y + FIELD_SIZE_PIXELS) {
+        if (InputHandler.getMouseX() < x || InputHandler.getMouseY() < y || InputHandler.getMouseX() >= x + FIELD_SIZE_PIXELS || InputHandler.getMouseY() >= y + FIELD_SIZE_PIXELS) {
             selCellX = -1;
             selCellY = -1;
         }
     }
 
     public boolean clickForStrike() {
-        if(InputHandler.isClicked() && selCellY > -1 && selCellX > -1 && !fieldIsMy())
-            if(fieldSet[selCellY][selCellX] == "water" || fieldSet[selCellY][selCellX] == "ship") {
-                gotStrike(selCellX,selCellY);
+        if (InputHandler.isClicked() && selCellY > -1 && selCellX > -1 && !fieldIsMy())
+            if (fieldSet[selCellY][selCellX] == "water" || fieldSet[selCellY][selCellX] == "ship") {
+                gotStrike(selCellX, selCellY);
                 return true;
-        }
+            }
         return false;
     }
 
@@ -82,10 +84,15 @@ public abstract class SeaField {
         }
         return fieldSet[y][x];
     }
+
     public boolean fieldIsMy() {
         return isMine;
     }
-    public String[][] showFieldSet() {
-        return fieldSet;
+
+    public boolean haveAliveShips() {
+        for (int i = 0; i < FIELD_SIZE; i++)
+            for (int j = 0; j < FIELD_SIZE; j++)
+                if (fieldSet[i][j] == "ship") return true;
+        return false;
     }
 }
